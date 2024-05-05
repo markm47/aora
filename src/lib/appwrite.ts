a4227f7587e02d6c98af427c4b0e648bc48f38c9
Mark Magee <markmagee.web.dev@gmail.com>
2024-05-05 21:09:49 +0100

    Added Appwrite Libraries

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,255 @@
+import {
+  Account,
+  Avatars,
+  Client,
+  Databases,
+  ID,
+  Query,
+  Storage,
+} from "react-native-appwrite";
+
+const {
+  APPWRITE_ENDPOINT,
+  APPWRITE_PROJECT,
+  APPWRITE_DATABASE_ID,
+  APPWRITE_USER_COLLECTION_ID,
+  APPWRITE_VIDEO_COLLECTION_ID,
+  APPWRITE_STORAGE_ID,
+} = process.env;
+
+const client = new Client();
+
+client
+  .setEndpoint(APPWRITE_ENDPOINT)
+  .setProject(APPWRITE_PROJECT)
+  .setPlatform('com.marks_dev.aora');
+
+const account = new Account(client);
+const storage = new Storage(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
+
+// Register user
+export async function createUser(email: string, password: string, username: string) {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      username
+    );
+
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(username);
+
+    await signIn(email, password);
+
+    const newUser = await databases.createDocument(
+      APPWRITE_DATABASE_ID,
+      APPWRITE_USER_COLLECTION_ID,
+      ID.unique(),
+      {
+        accountId: newAccount.$id,
+        email: email,
+        username: username,
+        avatar: avatarUrl,
+      }
+    );
+
+    return newUser;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Sign In
+export async function signIn(email: string, password) {
+  try {
+    const session = await account.createEmailSession(email, password);
+
+    return session;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Get Account
+export async function getAccount() {
+  try {
+    const currentAccount = await account.get();
+
+    return currentAccount;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Get Current User
+export async function getCurrentUser() {
+  try {
+    const currentAccount = await getAccount();
+    if (!currentAccount) throw Error;
+
+    const currentUser = await databases.listDocuments(
+      APPWRITE_DATABASE_ID,
+      APPWRITE_USER_COLLECTION_ID,
+      [Query.equal("accountId", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw Error;
+
+    return currentUser.documents[0];
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+// Sign Out
+export async function signOut() {
+  try {
+    const session = await account.deleteSession("current");
+
+    return session;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Upload File
+export async function uploadFile(file, type: string) {
+  if (!file) return;
+
+  const { mimeType, ...rest } = file;
+  const asset = { type: mimeType, ...rest };
+
+  try {
+    const uploadedFile = await storage.createFile(
+      APPWRITE_STORAGE_ID,
+      ID.unique(),
+      asset
+    );
+
+    const fileUrl = await getFilePreview(uploadedFile.$id, type);
+    return fileUrl;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Get File Preview
+export async function getFilePreview(fileId: string, type: string) {
+  let fileUrl;
+
+  try {
+    if (type === "video") {
+      fileUrl = storage.getFileView(APPWRITE_STORAGE_ID, fileId);
+    } else if (type === "image") {
+      fileUrl = storage.getFilePreview(
+        APPWRITE_STORAGE_ID,
+        fileId,
+        2000,
+        2000,
+        "top",
+        100
+      );
+    } else {
+      throw new Error("Invalid file type");
+    }
+
+    if (!fileUrl) throw Error;
+
+    return fileUrl;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Create Video Post
+export async function createVideoPost(form) {
+  try {
+    const [thumbnailUrl, videoUrl] = await Promise.all([
+      uploadFile(form.thumbnail, "image"),
+      uploadFile(form.video, "video"),
+    ]);
+
+    const newPost = await databases.createDocument(
+      APPWRITE_DATABASE_ID,
+      APPWRITE_VIDEO_COLLECTION_ID,
+      ID.unique(),
+      {
+        title: form.title,
+        thumbnail: thumbnailUrl,
+        video: videoUrl,
+        prompt: form.prompt,
+        creator: form.userId,
+      }
+    );
+
+    return newPost;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Get all video Posts
+export async function getAllPosts() {
+  try {
+    const posts = await databases.listDocuments(
+      APPWRITE_DATABASE_ID,
+      APPWRITE_VIDEO_COLLECTION_ID
+    );
+
+    return posts.documents;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Get video posts created by user
+export async function getUserPosts(userId: string) {
+  try {
+    const posts = await databases.listDocuments(
+      APPWRITE_DATABASE_ID,
+      APPWRITE_VIDEO_COLLECTION_ID,
+      [Query.equal("creator", userId)]
+    );
+
+    return posts.documents;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Get video posts that matches search query
+export async function searchPosts(query) {
+  try {
+    const posts = await databases.listDocuments(
+      APPWRITE_DATABASE_ID,
+      APPWRITE_VIDEO_COLLECTION_ID,
+      [Query.search("title", query)]
+    );
+
+    if (!posts) throw new Error("Something went wrong");
+
+    return posts.documents;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Get latest created video posts
+export async function getLatestPosts() {
+  try {
+    const posts = await databases.listDocuments(
+      APPWRITE_DATABASE_ID,
+      APPWRITE_VIDEO_COLLECTION_ID,
+      [Query.orderDesc("$createdAt"), Query.limit(7)]
+    );
+
+    return posts.documents;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
